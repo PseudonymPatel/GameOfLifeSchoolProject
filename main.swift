6399e628ee5a67c22e537f4b9b2fa89904c0e3d8
Sheen Patel <programmingpatel@gmail.com>
2019-02-26 08:18:25 -0500

    Update main.swift

--- main.swift
+++ main.swift
@@ -1,22 +1,39 @@
 var exit = false
-var colony = Colony(name:"temp", size:10)
-var colonys = [String:Colony]()
+var col = Colony(name:"temp", size:10)
+var colonys:[String:Colony] = ["temp":col]
+var current = "temp"
 while !exit {
 	print("\nCommand? ", terminator:"")
-	let input = splitStringIntoParts(readLine() ?? "")
-
+	var input = splitStringIntoParts(readLine() ?? "")
+	if input.count > 1 && (input[0] == "" || input[0] == " ") {
+		input.remove(at: 0)
+	}
 	switch input[0] {
 		case "create":
-			print("Create colony? Y/n")
+			var colony = colonys[current]!
+			print("Create new colony? Y/n")
 			let i = readLine() ?? "n"
 			if (i.uppercased() == "Y" || i.uppercased() == "YES") {
 				print("How big to make colony? ", terminator:"")
-				let input = Int(readLine() ?? "20")!
-				colony = Colony(name:"Your colony", size:input)
+				let inputnum = Int(readLine() ?? "20") ?? 20
+				print("name the colony", terminator:"")
+				guard let inputname:String = readLine(), inputname != "" else {
+					print("colony not created: no name")
+					break
+				}
+				if colonys.count > 1 {
+					colonys[inputname] = Colony(name:inputname, size:inputnum)
+				} else {
+					colonys[inputname] = Colony(name:inputname, size:inputnum)
+					colonys["temp"] = nil
+					current = inputname
+				}
+				print("colony size \(inputnum) created, name: \(inputname)")
 			} else {
 				print("did not create colony")
 			}
 		case "run":
+			let colony = colonys[current]!
 			if input.count >= 2, let times = Int(input[1]) {
 				colony.evolve(forTurns: times)
 			} else {
@@ -27,21 +44,43 @@ while !exit {
 				//colony.evolve(forTurns: Int(readLine()!)!)
 			}
 		case "add":
+			let colony = colonys[current]!
 			if input.count >= 3 && Int(input[1]) != nil && Int(input[2]) != nil {
 				colony.setCellAlive(Int(input[1])!, Int(input[2])!, now:true)
 			} else {
-				print("Enter point to add:", terminator: "")
-				let l = splitStringIntoParts(readLine()!)
-				if let hedi = Int(l[0]), let sunni = Int(l[1]) {
-					colony.setCellAlive(hedi, sunni, now:true)
-				}
+				//let simon = "" 
+				//while simon != "" {
+					print("Enter point to add:", terminator: "")
+					let l = splitStringIntoParts(readLine()!)
+					if let hedi = Int(l[0]), let sunni = Int(l[1]) {
+						colony.setCellAlive(hedi, sunni, now:true)
+					}
+				//}
 			}
 			print("current colony state:\n \(colony)")
+		case "display", "ls":
+			for (_, colony) in colonys {
+				print(colony)
+			}
+		case "current", "cd":
+			if input.count < 2 {
+                    print("use for command: `[cd/command] directory`")
+                }
+                
+                let oldCurry = current
+                current = input[1]
+                if colonys[current] != nil {
+                    print("Current is \(current)")
+                } else {
+                    print("There is no data set with this name. Type better.")
+                    current = oldCurry
+                }
+
 		case "help":
-			print("figur out urself chetar")
+			print("dispay - shows thing\nadd row col - adds at row col, if no param starts in interactive mode\nrun generations - runs x generations, if no param, starts in interactive\ncreate - prompts colony creation/overwrites current colony")
 		default:
 			print("https://youtu.be/LDU_Txk06tM")
 			print("mr. p has big g")
-			print("I know you are reading this hedi and i don't care im being cringy now shut the fuck up")
+			print("I know you are reading this hedi and i don't care im being cringy now shut the [REDACTED] up")
 	}
 }
